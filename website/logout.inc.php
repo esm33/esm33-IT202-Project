6009a2330ddd26cfda3ae1ecddec4dad68d6f9b1
Emily <esm33@njit.edu>
2026-02-13 14:34:24 -0500

    Phase 01 Skincare - esm33

--- website/logout.inc.php
+++ website/logout.inc.php
@@ -1,5 +1,13 @@
+
+
 <?php
 if (isset($_SESSION['login'])) {
     unset($_SESSION['login']);
+    unset($_SESSION['first_name']);
+    unset($_SESSION['last_name']);
+    unset($_SESSION['pronouns']);
+    unset($_SESSION['email_address']);
+    unset($_SESSION['phone_number']);
+    unset($_SESSION['name']);
 }
 header("Location: index.php");
